f0195a8f694e19763a0d989bc7dd6904857e4c91
Benjamin van der Veen <benjamin@survantjames.com>
2020-10-02 14:19:52 -0700

    support for application roles, groups (#9)
    * add DeleteRole, GetClientRole
    
    * remove stray nil from delete
    
    * add create/delete methods for groups

--- roles.go
+++ roles.go
@@ -8,19 +8,25 @@ import (
 const (
 	rolePath       = "/auth/admin/realms/:realm/roles"
 	roleByIDPath   = "/auth/admin/realms/:realm/roles-by-id/:id"
-	clientRolePath = "/auth/admin/realms/:realm/clients/:id/roles"
+	clientRolesPath = "/auth/admin/realms/:realm/clients/:id/roles"
+	clientRolePath = "/auth/admin/realms/:realm/clients/:id/roles/:role_id"
 )
 
 // GetClientRoles gets all roles for the realm or client
-func (c *Client) GetClientRoles(accessToken string, realmName, idClient string) ([]RoleRepresentation, error) {
+func (c *Client) GetClientRoles(accessToken string, realmName string, clientID string) ([]RoleRepresentation, error) {
 	var resp = []RoleRepresentation{}
-	var err = c.get(accessToken, &resp, url.Path(clientRolePath), url.Param("realm", realmName), url.Param("id", idClient))
+	var err = c.get(accessToken, &resp, url.Path(clientRolesPath), url.Param("realm", realmName), url.Param("id", clientID))
 	return resp, err
 }
 
 // CreateClientRole creates a new role for the realm or client
-func (c *Client) CreateClientRole(accessToken string, realmName, clientID string, role RoleRepresentation) (string, error) {
-	return c.post(accessToken, nil, url.Path(clientRolePath), url.Param("realm", realmName), url.Param("id", clientID), body.JSON(role))
+func (c *Client) CreateClientRole(accessToken string, realmName string, clientID string, role RoleRepresentation) (string, error) {
+	return c.post(accessToken, nil, url.Path(clientRolesPath), url.Param("realm", realmName), url.Param("id", clientID), body.JSON(role))
+}
+
+// DeleteRole deletes a role
+func (c *Client) DeleteRole(accessToken string, realmName string, clientID string, roleID string) error {
+	return c.delete(accessToken, url.Path(clientRolePath), url.Param("realm", realmName), url.Param("id", clientID), url.Param("role_id", roleID))
 }
 
 // GetRoles gets all roles for the realm or client
@@ -34,5 +40,12 @@ func (c *Client) GetRoles(accessToken string, realmName string) ([]RoleRepresent
 func (c *Client) GetRole(accessToken string, realmName string, roleID string) (RoleRepresentation, error) {
 	var resp = RoleRepresentation{}
 	var err = c.get(accessToken, &resp, url.Path(roleByIDPath), url.Param("realm", realmName), url.Param("id", roleID))
+	return resp, err
+}
+
+// GetClientRole gets a specific client role’s representation
+func (c *Client) GetClientRole(accessToken string, realmName string, clientID string, roleID string) (RoleRepresentation, error) {
+	var resp = RoleRepresentation{}
+	var err = c.get(accessToken, &resp, url.Path(clientRolePath), url.Param("realm", realmName), url.Param("id", clientID), url.Param("role_id", roleID))
 	return resp, err
 }
