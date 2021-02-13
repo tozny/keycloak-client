138726431d72373427d9d851a08edf7a03c95a31
Luke Woodward <luke@tozny.com>
2021-02-12 16:35:25 -0800

    fix comment

--- well_known.go
+++ well_known.go
@@ -8,7 +8,7 @@ const (
 	samlDescriptorPath = "/auth/realms/:realm/protocol/saml/descriptor"
 )
 
-// GetClientRoles gets all roles for the realm or client
+// GetSAMLDescriptor fetches the public XML IDP descriptor document for a realm
 func (c *Client) GetSAMLDescriptor(realmName string) (string, error) {
 	req := c.httpClient.Get()
 	resp, err := c.doRequest(req, url.Path(samlDescriptorPath), url.Param("realm", realmName))
